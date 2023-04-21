c8642496eacfcf74dbb0f4697e2d24c290dd9621
Rahul P S <rahulps995@gmail.com>
2023-04-21 22:29:45 +0530

    cors update to amplify

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    // origin: ["http://localhost:3000"],
+    origin: ["https://main.dyzo0pe2jo0xu.amplifyapp.com/login"],
     method: ["GET", "POST", "PATCH", "PUT"],
     credentials: true,
   })
